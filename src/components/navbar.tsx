136976536ebf7b2b73cb5074f14efbd4fa09bc73
Eric0506 <eric.zhou@commitsmart.com>
2024-03-21 22:11:50 +0800

    Fix responsive

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -14,7 +14,7 @@ const NavBar: FC<IProps> = ({page}) => {
           {page==='home'?(<li className="list-style list-style::after"><a className="a-text-style" href="#">Home</a></li>):(<li className="nav-manu-ul-li"><a href="/">Home</a></li>)}
           {page==='facilities'?(<li className="list-style list-style::after"><a className="a-text-style" href="#">Facilities</a></li>):(<li className="nav-manu-ul-li"><a href="/facilities">Facilities</a></li>)}
           {page==='rooms'?(<li className="list-style list-style::after"><a className="a-text-style" href="#">Rooms</a></li>):(<li className="nav-manu-ul-li"><a href="/rooms">Rooms</a></li>)}
-          {page==='contact_us'?(<li className="list-style list-style::after"><a className="a-text-style" href="#">Contact-us</a></li>):(<li className="nav-manu-ul-li"><a href="/contact_us">Contact-us</a></li>)}
+          {page==='contact_us'?(<li className="list-style list-style::after"><a className="a-text-style" href="#">Contact</a></li>):(<li className="nav-manu-ul-li"><a href="/contact_us">Contact</a></li>)}
         </ul>
       </nav>
     </>
